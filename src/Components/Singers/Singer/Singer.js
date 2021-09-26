5bfce58399ac0612ad3811f8cc518724b13681d1
MiaBacchu <jewelranabacchu@gmail.com>
2021-09-26 12:49:41 +0600

    font awesome added

--- src/Components/Singers/Singer/Singer.js
+++ src/Components/Singers/Singer/Singer.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import './Singer.css'
+import './Singer.css';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Singer = (props) => {
     // console.log(props.handleButton)
+    const element = <FontAwesomeIcon icon={faShoppingCart} />
     const { img, name, age, country, gender, rate } = props.singer
     return (
         <div className='singer-container'>
@@ -14,7 +17,7 @@ const Singer = (props) => {
             <h3>Rate : ${rate}</h3>
             <button
                 onClick={() => props.handleButton(props.singer)}
-                className="button">Add To Cart</button>
+                className="button">{element} Add To Cart</button>
 
         </div>
     );
